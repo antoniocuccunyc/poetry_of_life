04f90cc53734714fc7a40dbefe4a4c2e314ba5b2
antoniocuccunyc <iivanovaanton@gmail.com>
2026-08-12 21:45:48 -0400

    created cell, universe, render function, count neighbors

---
+++ target/debug/deps/game_of_life-0ab9fc1c32697e4e.d
@@ -0,0 +1,5 @@
+/Users/antoniocuccu/PyCharmMiscProject/rustGameOfLife/target/debug/deps/game_of_life-0ab9fc1c32697e4e.d: src/main.rs
+
+/Users/antoniocuccu/PyCharmMiscProject/rustGameOfLife/target/debug/deps/libgame_of_life-0ab9fc1c32697e4e.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/game_of_life.d
@@ -0,0 +1,1 @@
+/Users/antoniocuccu/PyCharmMiscProject/rustGameOfLife/target/debug/game_of_life: /Users/antoniocuccu/PyCharmMiscProject/rustGameOfLife/src/main.rs
